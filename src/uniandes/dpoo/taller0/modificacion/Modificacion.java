67ef6b80023d174c92251bfebadc91e9adbd74e8
Carlos <c.penaa@uniandes.edu.co>
2023-08-19 11:30:02 -0500

    Commit ULTRAFINAL (try/catch)

--- src/uniandes/dpoo/taller0/modificacion/Modificacion.java
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -7,9 +7,18 @@ import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
 import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
 
 public class Modificacion {
-	public static void main(String[] args) throws FileNotFoundException, IOException
+	public static void main(String[] args)
 	{
-		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		CalculadoraEstadisticas calc = null;
+		try {
+			calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println(calc.paisConMasMedallistas());
 	}
 }
